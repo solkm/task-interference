e4f4e38d2cbc2ac0dd94f938a0ea71c6421ac5c0
solkm <solkmarkman@gmail.com>
2024-01-23 11:07:44 -0500

    projection histogram

--- angle_plots.py
+++ angle_plots.py
@@ -10,6 +10,7 @@ Created on Mon Jan  8 13:38:41 2024
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from matplotlib import rcParams
 import os
 os.chdir('/Users/Sol/Desktop/CohenLab/DynamicTaskPerceptionProject/task-interference')
 
@@ -27,6 +28,7 @@ df2 = pd.read_pickle('./correct_choice_model/SH2_correctA_DCAdf.pkl')
 
 timepoints = np.arange(70, 121, 10)
 N_trialhists = min(df1.shape[0], df2.shape[0])
+aR_inds, aNR_inds = [], []
 
 angles_mat1 = np.zeros((N_trialhists, timepoints.shape[0]))
 dcovs_sl_mat1 = np.zeros((N_trialhists, timepoints.shape[0]))
@@ -45,18 +47,26 @@ for i in range(N_trialhists):
     angles_mat2[i] = angles_0to90(df2.loc[i, 'angles'])
     dcovs_sl_mat2[i] = df2.loc[i, 'dcovs_sl']
     dcovs_sf_mat2[i] = df2.loc[i, 'dcovs_sf']
+    
+    aR_inds.append(i) if df1.loc[i, 'reward_history'][-1] == 1 else aNR_inds.append(i)
+
+aR_inds, aNR_inds = np.array(aR_inds), np.array(aNR_inds)
 
 min_dcovs1 = np.minimum(dcovs_sl_mat1, dcovs_sf_mat1)
+max_dcovs1 = np.maximum(dcovs_sl_mat1, dcovs_sf_mat1)
+
 min_dcovs2 = np.minimum(dcovs_sl_mat2, dcovs_sf_mat2)
+max_dcovs2 = np.maximum(dcovs_sl_mat2, dcovs_sf_mat2)
 
 #%% angles histogram at one timepoint, dcov threshold
 
-tpt = 3
-bins = np.arange(0,91,3)
+tpt = 4
+bins = np.arange(0, 91, 3)
 bin_centers = (bins[1:] + bins[:-1])/2
 
-thresh = 0.2*np.mean(np.stack((dcovs_sl_mat1[:,0], dcovs_sf_mat1[:,0],
-                                dcovs_sl_mat2[:,0], dcovs_sf_mat2[:,0])))
+thresh_fac = 0.1
+thresh = thresh_fac*np.mean(np.stack((dcovs_sl_mat1[:,0], dcovs_sf_mat1[:,0],
+                                      dcovs_sl_mat2[:,0], dcovs_sf_mat2[:,0])))
 
 inds1 = np.where(min_dcovs1[:, tpt]>thresh)[0]
 inds2 = np.where(min_dcovs2[:, tpt]>thresh)[0]
@@ -64,14 +74,98 @@ inds2 = np.where(min_dcovs2[:, tpt]>thresh)[0]
 props1, _ = np.histogram(angles_mat1[inds1, tpt], bins=bins, density=True)
 plt.plot(bin_centers, props1, color='orange', label='monkey choice model')
 props2, _ = np.histogram(angles_mat2[inds2, tpt], bins=bins, density=True)
-plt.plot(bin_centers, props2, color='darkblue', label='correct choice model', 
-         zorder=0)
+plt.plot(bin_centers, props2, color='darkblue', label='correct choice model', zorder=0)
+median1 = np.median(angles_mat1[inds1, tpt])
+median2 = np.median(angles_mat2[inds2, tpt])
+y_med = np.round(max(np.max(props1), np.max(props2))*1.1, 2)
+plt.scatter(median1, y_med, color='orange', marker='v', label='median = %3.2f'%median1)
+plt.scatter(median2, y_med, color='darkblue', marker='v', label='median = %3.2f'%median2)
 plt.legend()
+plt.ylabel('Proportion of trials')
+plt.xlabel('Angle')
+plt.xticks(np.arange(0,91,15))
 
 print('%d ms into delay'%((timepoints[tpt]-timepoints[0])*10))
 print('threshold: %4.3f'%thresh)
 print('monkey model proportion of trials: %4.3f'%(inds1.shape[0]/N_trialhists))
 print('correct model proportion of trials: %4.3f'%(inds2.shape[0]/N_trialhists))
+
+rcParams['pdf.fonttype']=42
+rcParams['pdf.use14corefonts']=True
+#plt.savefig(f'./MM1_monkeyB1245_vs_SH2_correctA_angleshist_tpt{tpt}_threshfac{thresh_fac}.pdf', dpi=300, transparent=True)
+
+#%% alternative to thresholding: angles histogram weighted by dcovs
+
+def angles_weighted_counts(angles, weights, bins, norm_weights=True):
+
+    angles_dig = np.digitize(angles, bins)
+    weighted_counts = np.zeros(bins.shape[0]-1)
+
+    if norm_weights:
+        weights = weights/np.sum(weights)
+    for i in range(bins.shape[0]-1):
+        inds = np.where(angles_dig==i+1)[0]
+        weights_i = weights[inds]
+        weighted_counts[i] = np.sum(weights_i)
+
+    return weighted_counts
+
+# plot
+tpt = 4
+bins = np.arange(0, 91, 5)
+bin_centers = (bins[1:] + bins[:-1])/2
+weights1 = min_dcovs1[:, tpt]
+weights2 = min_dcovs2[:, tpt]
+weighted_counts1 = angles_weighted_counts(angles_mat1[:, tpt], weights1, bins)
+weighted_counts2 = angles_weighted_counts(angles_mat2[:, tpt], weights2, bins)
+
+plt.plot(bin_centers, weighted_counts1, color='orange', label='monkey choice model')
+plt.plot(bin_centers, weighted_counts2, color='darkblue', label='correct choice model')
+plt.legend()
+plt.xlabel('Angle')
+plt.ylabel('Weighted counts')
+
+#%% magnitude of projection of irrelevant onto relevant axis histogram
+
+proj1 = np.zeros((N_trialhists, timepoints.shape[0]))
+proj2 = np.zeros((N_trialhists, timepoints.shape[0]))
+
+for i in range(N_trialhists):
+    
+    proj1[i] = np.array([np.dot(df1.loc[i, 'ax_sl'][t], df1.loc[i, 'ax_sf'][t]) \
+                         for t in range(timepoints.shape[0])])
+    proj1[i] *= min_dcovs1[i]
+
+    proj2[i] = np.array([np.dot(df2.loc[i, 'ax_sl'][t], df2.loc[i, 'ax_sf'][t]) \
+                      for t in range(timepoints.shape[0])])
+    proj2[i] *= min_dcovs2[i]
+
+tpt = 4
+lim = 0.15
+binsize = 0.005
+bins = np.arange(0, lim+binsize, binsize)
+bin_centers = (bins[1:] + bins[:-1])/2
+
+props1_aNR, _ = np.histogram(np.abs(proj1[aNR_inds, tpt]), bins=bins, density=True)
+plt.plot(bin_centers, props1_aNR, color='orange', ls = '-', lw=2, zorder=3,
+         label='monkey choice model, after unrewarded trial')
+props2_aNR, _ = np.histogram(np.abs(proj2[aNR_inds, tpt]), bins=bins, density=True)
+plt.plot(bin_centers, props2_aNR, color='darkblue', ls = '-', lw=2, zorder=2,
+         label='correct choice model, after unrewarded trial')
+props1_aR, _ = np.histogram(np.abs(proj1[aR_inds, tpt]), bins=bins, density=True)
+plt.plot(bin_centers, props1_aR, color='orange', ls = '--', lw=2, zorder=1, 
+         label='monkey choice model, after rewarded trial')
+props2_aR, _ = np.histogram(np.abs(proj2[aR_inds, tpt]), bins=bins, density=True)
+plt.plot(bin_centers, props2_aR, color='darkblue', ls = '--', lw=2, zorder=0,
+         label='correct choice model, after rewarded trial')
+
+plt.legend()
+plt.xlabel('Magnitude of projection of irrelevant axis onto relevant axis')
+plt.ylabel('Probability density')
+
+rcParams['pdf.fonttype']=42
+rcParams['pdf.use14corefonts']=True
+#plt.savefig(f'./MM1_monkeyB1245_vs_SH2_correctA_projhist_aNRaR_tpt{tpt}.pdf', dpi=300, transparent=True)
 
 #%% angle vs time scatterplots
 
@@ -92,6 +186,3 @@ fig.supxlabel('Time (10 ms)')
 
 ax[0].set_title('monkey choice model')
 ax[1].set_title('correct choice model')
-
-#%%
-
