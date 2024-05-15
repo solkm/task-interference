15ddb73af3cf1e322632b4ca032fd656e4d276d3
solkm <solkmarkman@gmail.com>
2024-05-15 15:12:01 -0400

    contraction progress

--- contraction.py
+++ contraction.py
@@ -7,6 +7,7 @@ from psychrnn.backend.simulation import BasicSimulator
 from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
+from matplotlib import rcParams
 from matplotlib.colors import Normalize
 from model_behavior_functions import get_tasks
 
@@ -46,7 +47,7 @@ def get_test_inputs_longdelay2(tparams, delay_dur=500):
 
     return x_t
 
-#%% 
+#%% Load model weights and existing test data
 N_rec = 200
 
 # Load the model and monkey trial history params
@@ -54,14 +55,12 @@ name, testname, folder = \
     'MM1_monkeyB1245', 'alltestinds_noisevis0.8mem0.5rec0.1', 'monkey_choice_model'
     #'SH2_correctA', 'monkeyhist_alltestinds_noisevis0.8mem0.5rec0.1', 'correct_choice_model'
     #
-    
+print(name)
 weights_path = f'./{folder}/weights/{name}.npz'
 loaded_tparams = pickle.load(open(f'./{folder}/test_data/{name}_{testname}_trialparams.pickle', 'rb'))
 loaded_outputs = pickle.load(open(f'./{folder}/test_data/{name}_{testname}_modeloutput.pickle', 'rb'))
 modelchoices = np.argmax(loaded_outputs[:, -1, 2:6], axis=1) + 1
 modeltasks = get_tasks(modelchoices)
-SL_inds = np.where(modeltasks==1)[0]
-SF_inds = np.where(modeltasks==2)[0]
 
 # Get number of preceding rewards
 a1R, a1NR, a2R, a2NR = [], [], [], []
@@ -76,15 +75,15 @@ for i in range(loaded_tparams.shape[0]):
             a1NR.append(i)
         else:
             a2NR.append(i)
-
-taskoutdiff = loaded_outputs[:, -1, 0] - loaded_outputs[:, -1, 1] # SL - SF
+'''
+taskoutdiff_loaded = loaded_outputs[:, -1, 0] - loaded_outputs[:, -1, 1] # SL - SF
 plt.figure()
-plt.hist(taskoutdiff[a2R], bins=20, alpha=0.5, label='a2R')
-plt.hist(taskoutdiff[a1R], bins=20, alpha=0.5, label='a1R')
-plt.hist(taskoutdiff[a1NR], bins=20, alpha=0.5, label='a1NR')
-plt.hist(taskoutdiff[a2NR], bins=20, alpha=0.5, label='a2NR')
+plt.hist(taskoutdiff_loaded[a2R], bins=20, alpha=0.5, label='a2R')
+plt.hist(taskoutdiff_loaded[a1R], bins=20, alpha=0.5, label='a1R')
+plt.hist(taskoutdiff_loaded[a1NR], bins=20, alpha=0.5, label='a1NR')
+plt.hist(taskoutdiff_loaded[a2NR], bins=20, alpha=0.5, label='a2NR')
 plt.legend()
-
+'''
 #%% Define test inputs for long delay 1, reward hist conditions
 
 N_per_cond = 20
@@ -97,7 +96,6 @@ loaded_inds = np.concatenate((
     ))
 
 tparams = []
-colors = []
 for i, ind in  enumerate(loaded_inds):
     tparams.append({})
     tparams[i]['trial_ind'] = loaded_tparams[ind]['trial_ind']
@@ -106,23 +104,22 @@ for i, ind in  enumerate(loaded_inds):
     tparams[i]['dsl'] = loaded_tparams[ind]['dsl']
     tparams[i]['dsf'] = loaded_tparams[ind]['dsf']
 
+delaylabel = 'delay1'
 dur = 400
 test_inputs = get_test_inputs_longdelay1(tparams, delay_dur=dur)
 
 #%% Define test inputs for long delay 2, reward hist x stim1 conditions
 
-N_per_cond = 10
+N_per_cond = 20
 np.random.seed(2152)
 loaded_inds = np.concatenate((
-    np.random.choice(np.intersect1d(a2NR, SL_inds), N_per_cond, replace=False), 
-    np.random.choice(np.intersect1d(a2NR, SF_inds), N_per_cond, replace=False),
-    np.random.choice(np.intersect1d(a2R, SL_inds), N_per_cond, replace=False),
-    np.random.choice(np.intersect1d(a2R, SF_inds), N_per_cond, replace=False)
+    np.random.choice(a2NR, N_per_cond, replace=False), 
+    np.random.choice(a2R, N_per_cond, replace=False),
+    np.random.choice(a1NR, N_per_cond, replace=False),
+    np.random.choice(a1R, N_per_cond, replace=False)
     ))
 
-stim1s = [(2.5, 2.3), (2.5, 2.7), (2.3, 2.5), (2.7, 2.5)] # (SL, SF)
-cm_dict = {'a2NR_SL':cm['Reds'], 'a2NR_SF':cm['Purples'], 'a2R_SL':cm['Blues'], 'a2R_SF':cm['Greens']}
-colors = []
+stim1s = [(2.5, 2.1), (2.5, 2.9), (2.1, 2.5), (2.9, 2.5), (2.5, 2.5)] # (SL, SF)
 tparams = []
 for i, ind in  enumerate(loaded_inds):
     for j in range(len(stim1s)):
@@ -133,28 +130,12 @@ for i, ind in  enumerate(loaded_inds):
         tparams[t]['correct'] = loaded_tparams[ind]['correct']
         tparams[t]['dsf'] = loaded_tparams[ind]['dsf']
         tparams[t]['dsl'] = loaded_tparams[ind]['dsl']
-        tparams[t]['sf1'] = stim1s[j][0]
-        tparams[t]['sl1'] = stim1s[j][1]
-
-        # define colors
-        if i//N_per_cond == 0:
-            cmap = cm_dict['a2NR_SL']
-            x = (stim1s[j][0] - 2.0)
-            colors.append(cmap(x))
-        elif i//N_per_cond == 1:
-            cmap = cm_dict['a2NR_SF']
-            x = (stim1s[j][0] - 2.0)
-            colors.append(cmap(x))
-        elif i//N_per_cond == 2:
-            cmap = cm_dict['a2R_SL']
-            x = (stim1s[j][0] - 2.0)
-            colors.append(cmap(x))
-        elif i//N_per_cond == 3:
-            cmap = cm_dict['a2R_SF']
-            x = (stim1s[j][0] - 2.0)
-            colors.append(cmap(x))
-
-test_inputs = get_test_inputs_longdelay2(tparams, delay_dur=300)
+        tparams[t]['sl1'] = stim1s[j][0]
+        tparams[t]['sf1'] = stim1s[j][1]
+        
+delaylabel = 'delay2'
+dur = 400
+test_inputs = get_test_inputs_longdelay2(tparams, delay_dur=dur)
 
 #%% Run the simulation
 
@@ -173,45 +154,138 @@ model_output, state_var = simulator.run_trials(test_inputs)
 taskoutdiff_longdelay = model_output[:, -1, 0] - model_output[:, -1, 1] # SL - SF
 colors_taskoutdiff = cm.coolwarm((taskoutdiff_longdelay + 1)/2)
 
-# %% PCA for visualization
+# %% PCA for visualization, on all trials
 
-t_pca = np.arange(state_var.shape[1]-1, state_var.shape[1])
+tpca_label = 'last' #'stim1off' #
+t_pca = np.arange(state_var.shape[1]-1, state_var.shape[1]) # np.arange(70, 71) 
 sv_pca = state_var[:, t_pca, :]
 pca = PCA(n_components=3)
 pca.fit_transform(sv_pca.reshape(-1, sv_pca.shape[-1]))
 print(pca.explained_variance_ratio_)
 X = state_var @ pca.components_.T
 
-#%% 3D plot
+#%% 3D plot, all trials, color by task output difference
 
 #%matplotlib widget
 fig = plt.figure(figsize=(8, 8))
 ax = fig.add_subplot(111, projection='3d')
 for i in range(N_batch):
     c = colors_taskoutdiff[i]
-    ax.plot(X[i, 70:, 0], X[i, 70:, 1], X[i, 70:, 2], color=c, zorder=0)
-    '''
-    ax.scatter(X[i, 69, 0], X[i, 69, 1], X[i, 69, 2], marker='v', edgecolor='k',
-               color=c, s=50, alpha=1, zorder=1)
-    ax.scatter(X[i, 109, 0], X[i, 109, 1], X[i, 109, 2], marker='o', edgecolor='k', 
-               color=c, s=50, alpha=1, zorder=1)
-    '''
+    ax.plot(X[i, :, 0], X[i, :, 1], X[i, :, 2], color=c, zorder=0, alpha=0.25)
     ax.scatter(X[i, -1, 0], X[i, -1, 1], X[i, -1, 2], marker='X', edgecolor='k', 
-               color=c, s=50, alpha=1, zorder=1)
+               lw=0.5, color=c, s=80, alpha=1, zorder=1)
     
-# %%2D plot 
+#%% 2D plot, all trials, color by task output difference
 
 fig, ax = plt.subplots(figsize=(8, 8))
+rcParams['font.size'] = 12
 for i in range(N_batch):
     c = colors_taskoutdiff[i]
-    ax.plot(X[i, :, 0], X[i, :, 1], color=c, zorder=0, alpha=0.2)
+    ax.plot(X[i, :, 0], X[i, :, 1], color=c, zorder=0, alpha=0.25)
+    ax.scatter(X[i, 50, 0], X[i, 50, 1], marker='s', edgecolor='k', lw=0.4,
+               color=c, s=20, alpha=0.7, zorder=1)
     ax.scatter(X[i, -1, 0], X[i, -1, 1], marker='X', edgecolor='k', lw=0.4,
-               color=c, s=80, alpha=1, zorder=1)
+               color=c, s=80, alpha=1, zorder=2)
 ax.set_aspect('equal')
-ax.set(xlabel='PC1', ylabel='PC2')
+ax.set(xlabel='PC1', ylabel='PC2', 
+       xticks=np.arange(-8, 13, 4), yticks=np.arange(-12, 1, 4)
+       )
 cbar = plt.colorbar(cm.ScalarMappable(cmap=cm.coolwarm, norm=Normalize(-1, 1)), 
                     ax=ax, fraction=0.03, label='Task output difference (L - F)')
 cbar.set_ticks([-1, -0.5, 0, 0.5, 1])
+plt.tight_layout()
+#plt.savefig(f'./contraction_figs/{name}_{delaylabel}dur{dur}_tPCA{tpca_label}.png', dpi=300)
 
-#plt.savefig(f'./contraction_figs/{name}_delay1dur{dur}_tPCAlast.png', dpi=300)
-# %%
+#%% Choose a subset of task certainty conditions to plot
+
+np.random.seed(2152)
+diff_dig = np.digitize(taskoutdiff_longdelay, [-1, -0.8, -0.4, -0.2, 0.2, 0.4, 0.8, 1])
+toplot = [np.random.choice(np.intersect1d(np.where(diff_dig==i)[0], 
+            np.arange(0, diff_dig.shape[0], len(stim1s)))) for i in [3]] #[1, 3, 5, 7] 
+print(taskoutdiff_longdelay[toplot])
+
+# PCA on the trials defined by this subset
+tpca_label = 'stim1offplottedtrials'
+t_pca = np.arange(70, 71)
+pca_trials = np.concatenate([np.arange(i, i+len(stim1s)) for i in toplot])
+sv_pca = state_var[pca_trials, t_pca, :]
+pca = PCA(n_components=3)
+pca.fit_transform(sv_pca.reshape(-1, sv_pca.shape[-1]))
+print(pca.explained_variance_ratio_)
+X = state_var @ pca.components_.T
+
+#%% 2D plot for a subset of trials, color by relevant/irrelevant stim1
+
+fig, ax = plt.subplots(figsize=(8, 8))
+rcParams['font.size'] = 12
+colorby = 'rel' # 'irrel' #
+
+for i, ind in enumerate(toplot):
+
+    c1 = colors_taskoutdiff[ind]
+    rel = 'L' if taskoutdiff_longdelay[ind] > 0 else 'F'
+
+    ax.scatter(X[ind, 50, 0], X[ind, 50, 1], marker='s', edgecolor='k', 
+               lw=0.2, color=c1, s=60, alpha=1, zorder=1)
+
+    for j in range(len(stim1s)):
+
+        if (rel=='L' and colorby=='rel') or (rel=='F' and colorby=='irrel'):
+            c2 = cm.plasma((stim1s[j][0] - 2))
+            c3 = cm.plasma((stim1s[j][1] - 2))
+        elif (rel=='F' and colorby=='rel') or (rel=='L' and colorby=='irrel'):
+            c2 = cm.plasma((stim1s[j][1] - 2))
+            c3 = cm.plasma((stim1s[j][0] - 2))
+
+        ax.plot(X[ind+j, 50:, 0], X[ind+j, 50:, 1], color=c2, zorder=0, alpha=0.8)
+        ax.scatter(X[ind+j, 70, 0], X[ind+j, 70, 1], marker='>', edgecolor=c3, 
+                   lw=1.2, color=c2, s=80, alpha=1, zorder=2)
+        ax.scatter(X[ind+j, 110, 0], X[ind+j, 110, 1], marker='o', edgecolor=c3, 
+                   lw=1.2, color=c2, s=80, alpha=1, zorder=2)
+        ax.scatter(X[ind+j, -1, 0], X[ind+j, -1, 1], marker='X', edgecolor='k', 
+                   lw=0.2, color=c1, s=80, alpha=1, zorder=1)
+ 
+#ax.set_aspect('equal')
+ax.set(xlabel='PC1', ylabel='PC2')
+cbar = plt.colorbar(cm.ScalarMappable(cmap=cm.plasma, norm=Normalize(2, 3)), 
+                    ax=ax, fraction=0.02, label='Stimulus feature')
+cbar.set_ticks([2, 2.5, 3])
+plt.tight_layout()
+
+taskdiffs = np.round(taskoutdiff_longdelay[toplot], 3)
+#plt.savefig(f'./contraction_figs/{name}_{delaylabel}dur{dur}_tPCA{tpca_label}_{taskdiffs}.png', dpi=300)
+
+# %% 3D plot for a subset of trials, color by relevant/irrelevant stim1
+
+fig = plt.figure(figsize=(8, 8))
+ax = fig.add_subplot(111, projection='3d')
+rcParams['font.size'] = 12
+colorby = 'rel'
+# %matplotlib widget
+
+for i, ind in enumerate(toplot):
+
+    c1 = colors_taskoutdiff[ind]
+    rel = 'L' if taskoutdiff_longdelay[ind] > 0 else 'F'
+
+    ax.scatter(X[ind, 50, 0], X[ind, 50, 1], X[ind, 50, 2], marker='s', edgecolor='k', 
+               lw=0.4, color=c1, s=40, alpha=1, zorder=1)
+
+    for j in range(len(stim1s)):
+
+        if rel=='L' and colorby=='rel':
+            c2 = cm.plasma((stim1s[j][0] - 2))
+        elif rel=='F' and colorby=='rel':
+            c2 = cm.plasma((stim1s[j][1] - 2))
+
+        ax.plot(X[ind+j, 50:, 0], X[ind+j, 50:, 1], X[ind+j, 50:, 2], 
+                    color=c2, zorder=0, alpha=0.8)
+        ax.scatter(X[ind+j, 70, 0], X[ind+j, 70, 1], X[ind+j, 70, 2], 
+                    marker='>', edgecolor='k', lw=0.4, color=c2, s=40, alpha=1, zorder=1)
+        ax.scatter(X[ind+j, 110, 0], X[ind+j, 110, 1], X[ind+j, 110, 2], 
+            marker='o', edgecolor='k', lw=0.4, color=c2, s=40, alpha=1, zorder=1)
+        ax.scatter(X[ind+j, -1, 0], X[ind+j, -1, 1], X[ind+j, -1, 2], 
+                    marker='X', edgecolor='k', lw=0.4, color='grey', s=40, alpha=1, zorder=2)
+
+
+#%%
